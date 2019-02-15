e6f65aafa3ce98d5f85cc0cb085737f2bcdff397
Nik Ambukovski <lazokin@hotmail.com>
2019-02-15 22:27:07 +1100

    add templates and bootstrap

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,23 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+posts = [
+    {
+        'author': 'Jogn Doe',
+        'title': 'Blog Post 1',
+        'content': 'Cotent for Blog Post 1',
+        'date_posted': 'January 1, 1970'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Cotent for Blog Post 2',
+        'date_posted': 'January 1, 1970'
+    }
+]
+
 
 def home(request):
-    return HttpResponse('<h1>Home Page</h1>')
+    return render(request, 'blog/home.html', {'posts': posts})
 
 def about(request):
-    return HttpResponse('<h1>About Page</h1>')
+    return render(request, 'blog/about.html', {'title': 'About'})
